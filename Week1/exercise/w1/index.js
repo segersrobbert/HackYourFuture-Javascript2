187dc665ceee492f2fa313ccdd1f26731375734b
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2019-04-29 03:35:51 +0200

    Update index.js

--- Week1/exercise/w1/index.js
+++ Week1/exercise/w1/index.js
@@ -1,11 +1,22 @@
-console.log('Hack your future, right?!');
+console.log("Hack your future, right?!");
 
-const header = document.querySelector('h1');
-console.log('header: ', header);
+const header = document.querySelector("h1");
+console.log("header: ", header);
 
 // EXERCISE:
-// select "change image" button (querySelector)
-// add click event listener
+const selectIdChangeImageOfButton = document.querySelector("#btn-changeImage"); // select "change image" button (querySelector)
+console.log(selectIdChangeImageOfButton);
+const changeImage = () => {
+  const selectImage = document.querySelector("img");
+  const selectIdImageInput = document.getElementById("imageInput");
+  console.log("selectIdImageInput.value");
+  if (selectIdImageInput.value !== "") {
+    selectImage.src = selectIdImageInput.value;
+  }
+  selectIdImageInput.value = "";    
+};
+selectIdChangeImageOfButton.addEventListener("click", changeImage); // add click event listener
+
 // execute changeImage() function on click event
 // define changeImage() function
 // in this function:
@@ -17,6 +28,22 @@ console.log('header: ', header);
 // ====================================== //
 
 // BONUS:
+const addTodoButtonRef = document.querySelector("#btn-addTodo");
+console.log("addTodoButtonRef");
+const addTodo = () => {
+  const getTodoListUlRef = document.getElementById("todoList");
+  const getTodoInputRef = document.getElementById("todoInput");
+  console.log("getTodoInputRef.value");
+  const createListElement = document.createElement("li");
+  if (getTodoInputRef.value !== "") {
+    createListElement.innerHTML = getTodoInputRef.value;
+    getTodoListUlRef.appendChild(createListElement);
+  }
+
+  getTodoInputRef.value = "";
+};
+
+addTodoButtonRef.addEventListener("click", addTodo);
 // select "add todo" button (querySelector)
 // add click event listener
 // execute addTodo() function on click event

7832fa1c5ad1292f3ac87249e3e743d828260d5e
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-04-29 17:58:17 +0200

    10 books added with forloop

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,7 +1,7 @@
 //'use strict';
 
 {
-  const bookTitles = {
+  const books = {
     'feed_me_vegan': {
       title: 'Feed Me Vegan',
       author: 'Lucy Watson',
@@ -24,7 +24,7 @@
       title: 'How to Give Up Plastic',
       author: 'Will McCallum',
       status: 'Wishlist',
-      review: 'https://www.goodreads.com/book/show/38587839-how-to-give-up-plastic?ac=1&from_search=true'
+      review: 'https://www.goodreads.com/book/show/38587839-how-to-give-up-plastics'
     },
     'unlocking_italian_with_paul_noble': {
       title: 'Unlocking Italian with Paul Noble',
@@ -45,35 +45,63 @@
       review: 'https://www.goodreads.com/book/show/40408061-the-basics-of-user-experience-design'
     },
     'our_korean_kitchen': {
-      title: 'our_korean_kitchen',
-      author: ['Jordan Bourke', 'Rejina Pyo'],
+      title: 'Our Korean Kitchen',
+      author: ['Jordan Bourke', ' Rejina Pyo'],
       status: 'Read',
       review: 'https://www.goodreads.com/review/show/2802308115'
     },
     'good_night_stories_for_rebel_girls': {
       title: 'Good Night Stories for Rebel Girls',
-      author: ['Elena Favilli', 'Francesca Cavallo'],
+      author: ['Elena Favilli', ' Francesca Cavallo'],
       status: 'Currently reading',
       review: 'https://www.goodreads.com/review/show/2802308415'
     },
     'the_girl_who_takes_an_eye_for_an_eye': {
       title: 'The Girl Who Takes an Eye for an Eye',
-      author: 'David Lagercrantz,',
+      author: 'David Lagercrantz',
       status: 'Read',
       review: 'https://www.goodreads.com/review/show/2272703977'
     }
   };
+
+
   const myBooks = document.querySelector('.books');
   const myBook = document.getElementsByClassName('book');
-  for (var i = 0; i < bookTitles.length; i++) {
-    const createListElement = document.createElement('li');
-    createListElement.innerHTML = bookTitles.title[i];
-    createListElement;
-    myBooks.appendChild(createListElement);
-    console.log(createListElement);
-  }
+
   const addBook = () => {
-    console.log("hello");
+    for (const book in books) {
+      const createListElement = document.createElement('li');
+      myBooks.appendChild(createListElement);
+
+      const createBookTitle = document.createElement('h1');
+      createBookTitle.innerHTML = books[book].title;
+      createBookTitle.className = "bookTitle";
+      createListElement.appendChild(createBookTitle);
+
+
+      const createBookCover = document.createElement('img');
+      createBookCover.src = `./assets/${book}.jpg`;
+      createBookCover.alt = books[book].title;
+      createListElement.appendChild(createBookCover);
+
+      const createAuthor = document.createElement('span');
+      createAuthor.innerHTML = 'Author: ' + books[book].author;
+      createListElement.appendChild(createAuthor);
+
+      const createStatusReading = document.createElement('span');
+      createStatusReading.innerHTML = 'Status: ' + books[book].status;
+      createListElement.appendChild(createStatusReading);
+
+      const createReviewLink = document.createElement('a');
+      createReviewLink.href = books[book].review;
+      createReviewLink.target = "_blank";
+      createReviewLink.innerHTML = 'My Review';
+      createListElement.appendChild(createReviewLink);
+
+      console.log(`book.${book}`);
+      console.log(books[book].author);
+      console.log(books[book].title);
+    }
   }
   addBook();
 }

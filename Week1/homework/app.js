ff7df41822918dd49c68f8b44ba41ff1dfea2857
Nihan2019 <48648228+Nihan2019@users.noreply.github.com>
2019-04-30 22:04:36 +0200

    Update app.js

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -3,6 +3,16 @@
 {
   const bookTitles = [
     // Replace with your own book titles
+    'dan_brown_da_vinci_code',
+    'suzanne_collins_hunger_games',
+    'michelle_obama_becoming',
+    'harper_lee_to_kill_a_mockingbird',
+    'elif_shafak_forty_rules_of_love',
+    'orhan_pamuk_a_strangeness_inmymind',
+    'john_green_fault_in_our_stars',
+    'bill_clinton_my_life',
+    'antoine_de_saint_exupery_little_prince',
+    'geronimo_stilton_lost_treasure_ofthe_emerald_eye',
     'harry_potter_chamber_secrets',
   ];
 

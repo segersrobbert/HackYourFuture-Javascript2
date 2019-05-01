0888d57237a9bba5754cd6ff84621efc06de2eb2
sena <48647939+msenaavci@users.noreply.github.com>
2019-05-01 16:53:44 +0200

    Update app.js

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -2,8 +2,16 @@
 
 {
   const bookTitles = [
-    // Replace with your own book titles
+    'war_and_peace',
+    'crime_and_punishment',
+    "alice's_adventures_in_wonderland",
+    'pride_and_prejudice',
+    'anna_karenina',
+    'nineteen_eighty_four',
     'harry_potter_chamber_secrets',
+    'the_stranger',
+    'acımak',
+    'my_sweet_orange_tree'
   ];
 
   // Replace with your own code

d8f525fd1bc96f6e8d012c1c60b289d99ada0ce8
codelawer <codelawe@gmail.com>
2019-05-01 09:48:59 +0200

    Rewrite 01.05.2019

--- Week1/exercise/w1/index.js
+++ Week1/exercise/w1/index.js
@@ -10,36 +10,37 @@
 //    set image src to imageInput element value
 
 // ====================================== //
+//------------------GET AND CREATE ELEMENTS--------------------
+
 const ChangeImageButton = document.querySelector('#btn-changeImage');
 const imgElement = document.querySelector('#imageToChange');
 const imageInput = document.getElementById('imageInput');
+
+//------------------FUNCTION--------------------
+
+//------------------FIRST--------------------
+/*
 const changeImage = () => {
   imgElement.src = imageInput.value;
 };
 ChangeImageButton.addEventListener('click', changeImage); // in this ES6 new method this part should after the function
+*/
 
-//or
+//------------------SECOND--------------------
+
 /*
 ChangeImageButton.addEventListener('click', () => {
-  const imgElement = document.querySelector('#imageToChange');
-  const imageInput = document.getElementById('imageInput');
-  //console.log(imageInput.value);
   imgElement.src = imageInput.value;
 });
 */
 
-// or
-/*function changeImage() {
-  const imgElement = document.querySelector('#imageToChange');
-  const imageInput = document.getElementById('imageInput');
-  //console.log(imageInput.value);
+//------------------THIRD------------------
+
+function changeImage() {
   imgElement.src = imageInput.value;
 }
+ChangeImageButton.addEventListener('click', changeImage); //declare before or after function
 
-
-
-
-*/
 // BONUS:
 // select "add todo" button (querySelector)
 // add click event listener
@@ -53,12 +54,35 @@ ChangeImageButton.addEventListener('click', () => {
 //    set <li> element innerHtml to todoInput value
 //    add <li> element to todoList
 
+//------------------GET AND CREATE ELEMENTS--------------------
 const addTodoButton = document.getElementById('btn-addTodo');
 const getTodoList = document.querySelector('#todoList');
 const getTodoInput = document.querySelector('#todoInput');
 const createLiElement = document.createElement('li');
+
+//------------------FUNCTION--------------------
+
+//------------------FIRST--------------------
+
+/*
 const addTodo = () => {
   createLiElement.innerHTML = getTodoInput.value;
   getTodoList.appendChild(createLiElement);
 };
+addTodoButton.addEventListener('click', addTodo);
+*/
+
+//------------------SECOND--------------------
+/*
+addTodoButton.addEventListener('click', () => {
+  createLiElement.innerHTML = getTodoInput.value;
+  getTodoList.appendChild(createLiElement);
+});
+*/
+
+//------------------THIRD------------------
+function addTodo() {
+  createLiElement.innerHTML = getTodoInput.value;
+  getTodoList.appendChild(createLiElement);
+}
 addTodoButton.addEventListener('click', addTodo);

17d03caab12c5421fc5f0839331097c12b4f9c88
Frisca13 <48647150+Frisca13@users.noreply.github.com>
2019-05-03 21:16:02 +0200

    Update index.js

--- Week1/exercise/w1/index.js
+++ Week1/exercise/w1/index.js
@@ -1,30 +1,53 @@
 console.log('Hack your future, right?!');
 
-const header = document.querySelector('h1');
-console.log('header: ', header);
+const selectHeader = document.querySelector('h1');
+console.log('header: ', selectHeader);
 
 // EXERCISE:
 // select "change image" button (querySelector)
+const selectButtonToChangeImage = document.getElementById('btn-changeImage');
+
+// define changeImage() function
+const changeImage = () => {
+  // in this function:
+  //    get image element
+  const selectImage = document.querySelector('img');
+  //    get imageInput element
+  const selectImageInput = document.getElementById('imageInput');
+  //    log input value
+  console.log(selectImageInput.value);
+  //    set image src to imageInput element value
+  selectImage.src = selectImageInput.value;
+}
 // add click event listener
 // execute changeImage() function on click event
-// define changeImage() function
-// in this function:
-//    get image element
-//    get imageInput element
-//    log input value
-//    set image src to imageInput element value
+selectButtonToChangeImage.addEventListener('click', changeImage);
+
+
 
 // ====================================== //
 
 // BONUS:
 // select "add todo" button (querySelector)
+const getToDoButtonClick = document.querySelector('#btn-addTodo');
+console.log(getToDoButton);
+// define addTodo() function
+const addTodo = () => {
+  // in this function:
+  //    get todoList element
+  const getTodoList = document.querySelector('#todoList');
+  //    get todoInput element
+  const getTodoInput = document.querySelector('#todoInput');
+  //    log todoInput element value
+  console.log(getTodoInput.value);
+  //    create <li> element
+  const createListElement = document.createElement('li');
+  //    set <li> element innerHtml to todoInput value
+  createListElement.innerHTML = getTodoInput.value;
+  console.log(createListElement);
+  //    add <li> element to todoList
+  getTodoList.appendChild(createListElement);
+}
 // add click event listener
 // execute addTodo() function on click event
-// define addTodo() function
-// in this function:
-//    get todoList element
-//    get todoInput element
-//    log todoInput element value
-//    create <li> element
-//    set <li> element innerHtml to todoInput value
-//    add <li> element to todoList
+getToDoButtonClick.addEventListener('click', addTodo);

7c6c88e3cc2ef56d41940633add789b2528ad751
sena <48647939+msenaavci@users.noreply.github.com>
2019-05-03 17:40:32 +0200

    Update index.js

--- Week1/exercise/w1/index.js
+++ Week1/exercise/w1/index.js
@@ -3,6 +3,22 @@ console.log('Hack your future, right?!');
 const header = document.querySelector('h1');
 console.log('header: ', header);
 
+document.querySelector('#btn-changeImage');
+const btnNewImage = document.querySelector('#btn-changeImage');
+console.log(btnNewImage);
+
+function changeImage() { 
+const img = document.querySelector('#imageToChange');
+const imgInput = document.querySelector('#imageInput');
+console.log(imgInput.value);
+  img.src = imgInput.value;
+}
+
+btnNewImage.addEventListener('click', changeImage);
+
+
+
+
 // EXERCISE:
 // select "change image" button (querySelector)
 // add click event listener
@@ -15,6 +31,20 @@ console.log('header: ', header);
 //    set image src to imageInput element value
 
 // ====================================== //
+document.querySelector('#btn-addTodo');
+const btnToDo = document.querySelector('#btn-addTodo');
+console.log(btnToDo);
+
+function newToDo () {
+    const toDo = document.querySelector('#todoList');
+    const toDoInput = document.querySelector('#todoInput');
+    console.log(toDoInput.value);
+    var li = document.createElement('li');
+    li.innerHTML = toDoInput.value; 
+    const ul = document.querySelector('ul');  
+    ul.appendChild(li);
+}
+btnToDo.addEventListener('click', newToDo);
 
 // BONUS:
 // select "add todo" button (querySelector)
@@ -28,3 +58,5 @@ console.log('header: ', header);
 //    create <li> element
 //    set <li> element innerHtml to todoInput value
 //    add <li> element to todoList
+
+

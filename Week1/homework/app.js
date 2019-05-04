32b1d91dd5dcc6977eb72c0a2b9f86e40c2bc860
Frisca13 <48647150+Frisca13@users.noreply.github.com>
2019-05-04 06:02:41 +0200

    Update app.js

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,23 +1,57 @@
-<!-- replace this with your HTML content -->
-<!DOCTYPE!html>
- 
+'use strict';
 
-<html lang="en">
-<head>
-  <meta charset="utf-8">
+{
+// Replace with your own book titles
+const bookTitles = [
+'how_to_train_your_dragon',
+'the_sword_of_shannara',
+'awaken_the_giant_within',
+'born_survivor',
+'sapiens',
+'physics_of_imposibble',
+];
 
-  <title>Booklist</title>
-  <meta name="description" content="Booklist">
-  <meta name="author" content="Frisca Julia">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="style.css" />
 
-</head>
+const bookDetails = {
+'how_to_train_your_dragon': {
+  title: 'How To Train Your Dragon',
+  author: 'Cressida Cowell',
+  language: 'English'
+},
+'the_sword_of_shannara': {
+  title: 'The Sword Of Shannara',
+  author: 'Terry Brooks',
+  language: 'English'
+},
+'awaken_the_giant_within': {
+  title: 'Awaken The GIant WIthin',
+  author: 'Tony Robbins',
+  language: 'English'
+},
+'born_survivor': {
+  title: 'Born Survivor',
+  author: 'Bear Grylls',
+  language: 'English'
+},
+'sapiens': {
+  title: 'Sapiens',
+  author: 'Yuval Noah Harari',
+  language: 'English'
+},
+'physics_of_imposibble': {
+  title: 'Physics of Impossible',
+  author: 'Michio Kaku',
+  language: 'English'
+}
+};
 
-<body>
-  <header>Book List</header>                                                     
-  <ul></ul>                                                   
-</body>
-  <script src="app.js"></script>                                                    
-                                                      
-</html>
+const bookCovers = {
+cressida_cowell_how_to_train_your_dragon: './HOwToTrainYourDragonsColl.jpg',
+terry_brooks_the_sword_of_shannara: './TheSwordOfShannara.jpg',
+tony_robbins_awaken_the_giant_within: './AwakenTheGiantWithin.jpg',
+bear_grylls_born_survivor: './BornSurvivor.jpg',
+yuval_noah_harari_sapien: './Sapiens.jpg',
+michio_kaku_physics_of_imposibble: './PhysicsOfImpossible.jpg',
+ };
+
+

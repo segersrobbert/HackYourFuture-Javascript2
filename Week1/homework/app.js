d369f882b75420b572e0bcaa9775ce1d675b46fa
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-05-05 19:55:13 +0200

    the books array is removed, replaced with an obj

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,89 +1,5 @@
 'use strict';
 
-  // This is an array that stores my books' information.
-  // Each element is an object that contains details.
-  const books = [
-    {
-      id: 'feed_me_vegan',
-      title: 'Feed Me Vegan',
-      author: 'Lucy Watson',
-      status: 'Read',
-      review: 'https://www.goodreads.com/review/show/2660591037',
-      rating: 3
-    },
-    {
-      id: 'goodbye_things_on_minimalist_living',
-      title: 'Goodbye Things on Minimalist Living',
-      author: 'Fumio Sasaki',
-      status: 'Read',
-      review: 'https://www.goodreads.com/review/show/2660601002',
-      rating: 5
-    },
-    {
-      id: 'turning_the_tide_on_plastic',
-      title: 'Turning The Tide on Plastic',
-      author: 'Lucy Siegle',
-      status: 'Read',
-      review: 'https://www.goodreads.com/review/show/2662833090',
-      rating: 4
-    },
-    {
-      id: 'how_to_give_up_plastic',
-      title: 'How to Give Up Plastic',
-      author: 'Will McCallum',
-      status: 'Wishlist',
-      review: 'https://www.goodreads.com/book/show/38587839-how-to-give-up-plastics',
-    },
-    {
-      id: 'unlocking_italian_with_paul_noble',
-      title: 'Unlocking Italian with Paul Noble',
-      author: 'Paul Noble',
-      status: 'Read',
-      review: 'https://www.goodreads.com/review/show/2683716195',
-      rating: 2
-    },
-    {
-      id: 'wtf_should_i_eat',
-      title: 'Food: What the Heck Should I Eat?',
-      author: 'Mark Hyman',
-      status: 'Read',
-      review: 'https://www.goodreads.com/review/show/2762127902',
-      rating: 3
-    },
-    {
-      id: 'the_basics_of_user_experience_design',
-      title: 'The Basics of User Experience Design',
-      author: 'Mads Soegaard',
-      status: 'Currently reading',
-      review: 'https://www.goodreads.com/book/show/40408061-the-basics-of-user-experience-design'
-    },
-    {
-      id: 'our_korean_kitchen',
-      title: 'Our Korean Kitchen',
-      author: ['Jordan Bourke', ' Rejina Pyo'],
-      status: 'Read',
-      review: 'https://www.goodreads.com/review/show/2802308115',
-      rating: 4
-    },
-    {
-      id: 'good_night_stories_for_rebel_girls',
-      title: 'Good Night Stories for Rebel Girls',
-      author: ['Elena Favilli', ' Francesca Cavallo'],
-      status: 'Read',
-      review: 'https://www.goodreads.com/review/show/2802308415',
-      rating: 4
-    },
-    {
-      id: 'the_girl_who_takes_an_eye_for_an_eye',
-      title: 'The Girl Who Takes an Eye for an Eye',
-      author: 'David Lagercrantz',
-      status: 'Read',
-      review: 'https://www.goodreads.com/review/show/2272703977',
-      rating: 4
-    }
-  ];
-
-
 const booksObj = {
   feed_me_vegan: {
     id: 'feed_me_vegan',
@@ -166,7 +82,6 @@ const booksObj = {
 };
 
 const booksArray = Object.keys(booksObj).map(key => booksObj[key]);
-console.log(booksArray);
 
 // This function is used by compare(), the task is to set the "status" priority.
 // I want "Read" comes first, then "Currently reading" and "Wishlist"
@@ -203,11 +118,6 @@ function compare(a, b){
 
 }
 const myBooks = document.querySelector('.books');
-
-// I tried using filter(),
-// Result: not showing all the books I have.
-// It's handy if you want to only show the filtered books
-// const rankedBooks = books.filter(book => book.rating > 5 || book.rating === undefined);
 
 // Keep in mind that sort() only works for sorting an array, not an object.
 // We need to explain to the sort method how the objects need to be sorted using

53dbc2ac49538112bd0b98a1e6ff9ea1c1d24fa6
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-05-06 21:52:18 +0200

    added map-filter & maartjes-work homeworks

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -46,16 +46,18 @@ const maartjesTasks = monday.concat(tuesday);
 const maartjesHourlyRate = 20;
 
 function computeEarnings(tasks, hourlyRate) {
-  // Replace this comment and the next line with your code
-  console.log(tasks, hourlyRate);
+  return maartjesTasks.map(task => task.duration)
+  .map(durationInMinutes => durationInMinutes/60)
+  .filter(durationInHour => durationInHour >= 2)
+  .map(durationInHour => durationInHour * maartjesHourlyRate)
+  .reduce((sum, currentSalary) => sum + currentSalary)
+  .toFixed(2);
 }
-
 // eslint-disable-next-line no-unused-vars
 const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
 
 // add code to convert `earnings` to a string rounded to two decimals (euro cents)
-
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+console.log(`Maartje has earned €${earnings}`);
 
 // Do not change or remove anything below this line
 module.exports = {

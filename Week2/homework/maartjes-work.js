1da81ba0bd1fcc66a2cf923208c4b6e8bcf99852
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2019-05-07 16:51:53 +0200

    Update maartjes-work.js

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -42,12 +42,20 @@ const tuesday = [
   },
 ];
 
+
 const maartjesTasks = monday.concat(tuesday);
+console.log(maartjesTasks);
 const maartjesHourlyRate = 20;
 
 function computeEarnings(tasks, hourlyRate) {
-  // Replace this comment and the next line with your code
-  console.log(tasks, hourlyRate);
+    // Replace this comment and the next line with your code
+    //STEP 1-A new array created from the durations
+    const durationsInHours = tasks.map(elem => elem.duration / 60);
+    //STEP 2- The durations less than 2 hours are filtered out.
+    const durationsMoreThan2 = durationsInHours.filter(elem => elem >= 2);
+    //STEP 3- Multiply the each duration by a per-hour rate for billing (use €20/hour) and sum it all up.
+    const earningsMoreThan2 = durationsMoreThan2.reduce((acc, val) => acc + val * hourlyRate,0);
+    return earningsMoreThan2;
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -55,11 +63,11 @@ const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
 
 // add code to convert `earnings` to a string rounded to two decimals (euro cents)
 
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+console.log(`Maartje has earned "€${earnings.toFixed(2)}"`);
 
 // Do not change or remove anything below this line
 module.exports = {
-  maartjesTasks,
-  maartjesHourlyRate,
-  computeEarnings,
+    maartjesTasks,
+    maartjesHourlyRate,
+    computeEarnings
 };

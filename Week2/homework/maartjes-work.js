9d70f6f874780e7daf2709b8a0057b0ad8fa308e
Fatih Tutar <47762263+fatihtutar@users.noreply.github.com>
2019-05-10 00:04:22 +0200

    Update maartjes-work.js

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -1,5 +1,22 @@
 'use strict';
 
+function doubleOddNumbers(numbers) {
+  // Replace this comment and the next line with your code
+  return numbers.filter(val => val % 2 !== 0)
+    .map(val => val * 2);
+  console.log(numbers);
+}
+
+const newNumbers = [1, 2, 3, 4];
+console.log(doubleOddNumbers(myNumbers));
+
+// Do not change or remove anything below this line
+module.exports = {
+  myNumbers,
+  doubleOddNumbers,
+};
+
+
 const monday = [
   {
     name: 'Write a summary HTML/CSS',

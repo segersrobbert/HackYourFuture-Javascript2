c867da46eed560ccd82535393ecea9f6e249fabc
Fatih Tutar <47762263+fatihtutar@users.noreply.github.com>
2019-05-10 00:18:15 +0200

    Update map-filter.js

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 function doubleOddNumbers(numbers) {
   // Replace this comment and the next line with your code
@@ -6,6 +6,23 @@ function doubleOddNumbers(numbers) {
 }
 
 const myNumbers = [1, 2, 3, 4];
+console.log(doubleOddNumbers(myNumbers));
+
+// Do not change or remove anything below this line
+module.exports = {
+  myNumbers,
+  doubleOddNumbers,
+};*/
+'use strict';
+
+function doubleOddNumbers(numbers) {
+  // Replace this comment and the next line with your code
+  return numbers.filter(val => val % 2 !== 0)
+    .map(val => val * 2);
+  console.log(numbers);
+}
+
+const newNumbers = [1, 2, 3, 4];
 console.log(doubleOddNumbers(myNumbers));
 
 // Do not change or remove anything below this line

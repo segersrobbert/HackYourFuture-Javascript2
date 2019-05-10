2b842f2843deec2a5a2d5842747415192d3d49dc
Nihan2019 <48648228+Nihan2019@users.noreply.github.com>
2019-05-10 11:24:09 +0200

    Create JSON-exercises.js

---
+++ Week2/homework/JSON-exercises.js
@@ -0,0 +1,19 @@
+//Write a JavaScript function to check if an object contains given property.
+```js
+const testOfProperty = (object,prop) => 
+  object.hasOwnProperty(prop);
+
+const newObject = {tina:'dog',waffle:'cat',duman:'rabbit'};
+console.log(newObject.hasOwnProperty('waffle'));
+
+```
+---
+
+//Write a JavaScript program to get the length (amount of keys) of a JavaScript object
+
+```js
+const lengthOfObject = obj => Object.keys(obj).length;
+
+const newObject = {1:'a',2:'b',3:'c'};
+console.log(lengthOfObject(newO
+```

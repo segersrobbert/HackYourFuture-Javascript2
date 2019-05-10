9c89a089be19e38bd2c3ae7f65d0349d5ac740bf
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-05-10 23:19:03 +0200

    JSON exercise

--- Week2/exercises/jsonExercise.js
+++ Week2/exercises/jsonExercise.js
@@ -11,3 +11,11 @@ var getTheObjLength = key => {
   return Object.keys(key).length;
 };
 //Write a JavaScript program to create a Clock. Console, every second :”14:37:42”,”14:37:43", “14:37:44”, "14:37:45"
+var clock = () => {
+  var time = new Date();
+  var hours = time.getHours();
+  var minutes = time.getMinutes();
+  var seconds = time.getSeconds();
+  console.log(hours + ":" + minutes + ":" + seconds);
+}
+window.setInterval(clock, 1000);

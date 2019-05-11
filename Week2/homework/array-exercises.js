719f1dac41c5893d86928f48f08d711e917829bf
Nihan2019 <48648228+Nihan2019@users.noreply.github.com>
2019-05-11 17:17:32 +0200

    Update array-exercises.js

--- Week2/homework/array-exercises.js
+++ Week2/homework/array-exercises.js
@@ -4,37 +4,33 @@
 
 //Write a JavaScript function to get the first element of an array
 
-//```js
 //const myArray = [0,1,2,3,4,5,6];
-
 //const firstElement = arr => arr.shift();
 //console.log(firstElement(myArray));
-//```
+
 //---
 
 //Passing a parameter 'n' will return the first 'n' elements of the array.
-//```js
+
 //const myArray = [0,1,2,3,4,5,6];
 //const firstnElements = (arr,n) => arr.slice(0,n);
 //console.log(firstnElements(myArray,4));
-//```
 //get elements by default parameter
-```js
+
     
 const getElements = (arr, n = 1) => arr.slice(0, n);
 console.log(getElements(arr1));
 console.log(getElements(arr1, 3));
-```
+
 ---
 //Write a JavaScript program to find the most frequent item of an array.
-```js
 
 
-```
+
 ---
 
 //Write a JavaScript program which accept a string as input and swap the case of each character
-```js
 
-```
+
+
 ---

6424ce1e9074d59a8c4032bbaaf33d41aac91c01
Fatih Tutar <47762263+fatihtutar@users.noreply.github.com>
2019-05-12 05:32:11 +0200

    Update maartjes-work.js

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -48,16 +48,15 @@ const maartjesHourlyRate = 20;
 function computeEarnings(tasks, hourlyRate) {
   // Replace this comment and the next line with your code
   //Map the tasks to durations in hours.
-  const durations= maartjesTasks.map(tasks=> tasks.duration);
-  const durationsInHours = durations.map(durations =>durations/60);
+  return maartjesTasks.map(tasks=> tasks.duration);
+  .map(durations =>durations/60);
   //Filter out everything that took less than two hours (i.e., remove from the collection)
-  const plus2Hours = durationsInHours.filter( val => val >= 2);
+  .filter( val => val >= 2);
   //Multiply the each duration by a per-hour rate for billing (use €20/hour) and sum it all up.
-  const totalSum =plus2Hours.reduce(function (accumulator, currentValue) {
-     return accumulator + (currentValue * maartjesHourlyRate);;
-     }, 0);
+  .reduce((accumulator, currentValue) => accumulator + (currentValue * hourlyRate));
+     };
   // add code to convert `earnings` to a string rounded to two decimals (euro cents)
-  return totalSum.toFixed(2);
+  .toFixed(2);
   console.log(tasks, hourlyRate);
 }
   // eslint-disable-next-line no-unused-vars

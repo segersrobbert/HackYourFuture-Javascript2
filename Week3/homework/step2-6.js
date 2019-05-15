e67bfbc7dada254642318c77358aeb39a5f7fb9f
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2019-05-15 15:29:52 +0200

    Update step2-6.js

--- Week3/homework/step2-6.js
+++ Week3/homework/step2-6.js
@@ -1,20 +1,50 @@
 'use strict';
 
-const arr2d = [[1, 2], [3, 4], [5, 6]];
-const arr3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+const arr2d = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+];
+const arr3d = [
+    [
+        [1, 2],
+        [3, 4]
+    ],
+    [
+        [5, 6],
+        [7, 8]
+    ]
+];
 
 function flattenArray2d(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let x = 0; x < arr[i].length; x++) {
+            newArr.push(arr[i][x])
+        }
+    }
+
+    console.log(newArr);
+    return newArr;
 }
 
 function flattenArray3d(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let x = 0; x < arr[i].length; x++) {
+            for (let y = 0; y < arr[x].length; y++) {
+                newArray.push(arr[i][x][y]);
+            }
+        }
+    }
+
+    console.log(arr);
+    return newArray;
 }
 
 console.log(flattenArray2d(arr2d)); // -> [1, 2, 3, 4, 5, 6]
 console.log(flattenArray3d(arr3d)); // -> [1, 2, 3, 4, 5, 6, 7, 8]
+
 
 // Do not change or remove anything below this line
 module.exports = {

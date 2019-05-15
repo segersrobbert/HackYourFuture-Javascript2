a5f58e9d469d0d46cea6bef3dceb27e7e41d2234
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2019-05-15 16:43:47 +0200

    Update step2-2.js

--- Week3/homework/step2-2.js
+++ Week3/homework/step2-2.js
@@ -1,23 +1,33 @@
 'use strict';
 
-function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
-  const numbers = [];
 
-  // Replace this comment and the next line with your code
-  console.log(startIndex, stopIndex, threeCallback, fiveCallback, numbers);
+function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
+    const numbers = [];
+    for (let i = startIndex; i <= stopIndex; i++) {
+        numbers.push(i);
+        if (i % 3 === 0) {
+            threeCallback(i);
+        }
+        if (i % 5 === 0) {
+            fiveCallback(i);
+        }
+    }
+
+
+    console.log(numbers);
 }
 
+
+
+
 function sayThree(number) {
-  // Replace this comment and the next line with your code
-  console.log(number);
+    console.log(`${number} is divisible by 3`);
+
 }
 
 function sayFive(number) {
-  // Replace this comment and the next line with your code
-  console.log(number);
+    console.log(`${number} is divisible by 5`);
+
 }
 
 threeFive(10, 15, sayThree, sayFive);
-
-// Do not change or remove anything below this line
-module.exports = threeFive;

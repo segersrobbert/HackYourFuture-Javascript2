92135829bcacf6470887399d6562ed0296c4b3df
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2019-05-15 16:41:07 +0200

    Update step2-1.js

--- Week3/homework/step2-1.js
+++ Week3/homework/step2-1.js
@@ -1,13 +1,14 @@
-'use strict';
+"use strict";
 
 function foo(func) {
   // What to do here?
-  // Replace this comment and the next line with your code
-  console.log(func);
+
+  func();
+
 }
 
 function bar() {
-  console.log('Hello, I am bar!');
+  console.log("Hello, I am bar!");
 }
 
 foo(bar);

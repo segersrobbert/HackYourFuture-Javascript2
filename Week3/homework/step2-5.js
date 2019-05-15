9fc620ab458b6190bf3193af2c274e6244745720
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2019-05-15 16:47:39 +0200

    Update step2-5.js

--- Week3/homework/step2-5.js
+++ Week3/homework/step2-5.js
@@ -1,16 +1,26 @@
 'use strict';
 
 function multiplyAll(arr) {
-  // eslint-disable-next-line
-  let product = 1;
+    // eslint-disable-next-line
+    let product = 1;
+    for (let i = 0; i < arr.length; i++) {
+        for (let y = 0; y < arr[i].length; y++) {
+            product *= arr[i][y];
+        }
+    }
 
-  // Replace this comment and the next line with your code
-  console.log(arr, product);
 
-  return product;
+    console.log(arr, product);
+
+    return product;
 }
 
-const result = multiplyAll([[1, 2], [3, 4], [5, 6]]);
+
+const result = multiplyAll([
+    [1, 2],
+    [3, 4],
+    [5, 6]
+]);
 console.log(result); // 720
 
 // Do not change or remove anything below this line

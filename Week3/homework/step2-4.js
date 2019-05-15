2cc657eaf74ceb5399aea90c9564d82ead3cc9a1
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2019-05-15 15:27:13 +0200

    Update step2-4.js

--- Week3/homework/step2-4.js
+++ Week3/homework/step2-4.js
@@ -1,10 +1,14 @@
 'use strict';
 
+
 function Dog() {
-  // add your code here
+    // add your code here
+    this.name = "Rocky";
+    this.color = "grey";
+    this.numLegs = 4;
 }
 
 const hound = new Dog();
-
+console.log(hound);
 // Do not change or remove anything below this line
 module.exports = hound;

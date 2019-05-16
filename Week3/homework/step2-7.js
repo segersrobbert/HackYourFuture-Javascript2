af33d95bb6d2fc04955accc77c38f47209437168
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2019-05-16 14:31:44 +0200

    Update step2-7.js

--- Week3/homework/step2-7.js
+++ Week3/homework/step2-7.js
@@ -1,23 +1,37 @@
 'use strict';
-
+//x is a variable and a value of x is a number
 const x = 9;
+
+//declaring a function whose name is f1 and parameter val
+// and f1 function is Passed by Value.
+//f1 function is  passing the value of x variable as the argument.
+//This function is returning val= 10, but this is not accessible outside the function. (Private) 
 function f1(val) {
-  val = val + 1;
-  return val;
+    val = val + 1;
+    return val;
 }
 
 f1(x);
 
 console.log(x);
 
-const y = { x: 9 };
+const y = {
+    x: 9
+};
+
 function f2(val) {
-  val.x = val.x + 1;
-  return val;
+    val.x = val.x + 1;
+    return val;
 }
 
 f2(y);
 
 console.log(y);
 
-// Add your explanation as a comment here
+
+//The second function changes the value of y to y={x:10}. This is accessible also outside of the function. (Global variable)
+//Because objects are passed by reference.
+//Object outside a function is passed into a function by giving a reference to the outside object. When we use that 
+//reference to manipulate its object, the object outside is thus affected. However, if inside the function we decided 
+//to point the reference to something else, we did not affect the object outside at all, because all we did was 
+//re-direct the reference to something else.

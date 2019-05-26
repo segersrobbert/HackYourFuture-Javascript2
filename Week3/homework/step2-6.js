be7edcea6f4436e86e269d3d8fb3796468418d07
Yousef <46559177+yousefnk@users.noreply.github.com>
2019-05-26 08:18:17 +0200

    Update step2-6.js

--- Week3/homework/step2-6.js
+++ Week3/homework/step2-6.js
@@ -4,13 +4,27 @@ const arr2d = [[1, 2], [3, 4], [5, 6]];
 const arr3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 
 function flattenArray2d(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+  var flattend = [];
+  !(function flat(arr) {
+    arr.forEach(function(el) {
+      if (Array.isArray(el)) flat(el);
+      else flattend.push(el);
+    });
+  })(arr);
+  return flattend;
+
 }
 
 function flattenArray3d(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+  var flattend = [];
+  !(function flat(arr) {
+    arr.forEach(function(el) {
+      if (Array.isArray(el)) flat(el);
+      else flattend.push(el);
+    });
+  })(arr);
+  return flattend;
+
 }
 
 console.log(flattenArray2d(arr2d)); // -> [1, 2, 3, 4, 5, 6]

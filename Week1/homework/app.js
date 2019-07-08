29b4f512eeee227a663149f102d4fbaf58b4b908
Nelson Najarro <nelson.najarro@gmail.com>
2019-07-08 18:18:17 +0200

    Update books and testing eslint

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,11 +1,17 @@
 'use strict';
-
 {
   const bookTitles = [
-    // Replace with your own book titles
-    'harry_potter_chamber_secrets',
+    'the_old_man_and_the_sea',
+    'uncle_toms_cabin',
+    'twenty_love_poems_and_a_song_of_despair',
+    'romeo_and_juliet',
+    'hamlet',
+    'the_odyssey ',
+    'oedipus_the_king',
+    'the_clever_gentleman_sir_quixote_of_la_mancha',
+    'one_hundred_years_of_solitude',
+    'angels_and_demons',
   ];
 
-  // Replace with your own code
   console.log(bookTitles);
 }

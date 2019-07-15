f42786f5ae5f433176cf7f2072e31eb1338a3ba8
hakanErgin <hakan.ergin@gmail.com>
2019-07-15 09:50:04 +0200

    exercises folder added

---
+++ Week2/exercise/scripts.js
@@ -0,0 +1,24 @@
+const anArray = [
+    "the_vegetarian",
+    "watership_down",
+    "adventures_sherlock_holmes",
+    "alchemist",
+    "frankenstein",
+    "city_of_thieves",
+    "gone_with_the_wind",
+    "to_kill_mocking_bird",
+    "war_and_peace",
+    "things_fall_apart"
+];
+
+/* function get1stElement(arrItem) {
+    let firstElem = arrItem.shift();
+    return firstElem;
+}
+
+let returnedVal = get1stElement(anArray);           //// do it with [0] instead of shift not to change the org array
+console.log(returnedVal); */
+
+function getElems() {
+
+}

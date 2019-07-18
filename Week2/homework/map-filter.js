bf28ecc2668a721a0ebe894d5655292761356c4e
hakanErgin <hakan.ergin@gmail.com>
2019-07-18 13:04:20 +0200

    +map filter (updated)

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,15 +1,14 @@
 'use strict';
-
-function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
-}
-
 const myNumbers = [1, 2, 3, 4];
-console.log(doubleOddNumbers(myNumbers));
+const filteredNums = myNumbers.filter(num => num % 2 !== 0);
+//console.log(filteredNums);
+
+const doubleOddNumbers = filteredNums.map(num => num * 2)
+console.log(doubleOddNumbers);
 
 // Do not change or remove anything below this line
 module.exports = {
   myNumbers,
   doubleOddNumbers,
 };
+

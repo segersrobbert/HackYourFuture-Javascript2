9caa3744664cebe00a02d996ed6be9327da380dd
fgsumer <50924016+fgsumer@users.noreply.github.com>
2019-07-19 17:40:15 +0200

    Update map-filter.js

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -2,7 +2,9 @@
 
 function doubleOddNumbers(numbers) {
   // Replace this comment and the next line with your code
-  console.log(numbers);
+  //console.log(numbers);
+  var newNumbers = numbers.filter(x => x % 2 !== 0).map(x => x * 2);
+    return newNumbers;
 }
 
 const myNumbers = [1, 2, 3, 4];

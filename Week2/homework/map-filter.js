b8943f774549ac22b02c2fae30667a62411c07ce
prgrmfl <51049669+prgrmfl@users.noreply.github.com>
2019-07-20 11:07:36 +0300

    Add files via upload

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,12 +1,11 @@
 'use strict';
 
 function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
+  return numbers.filter(numb => numb % 2 === 1).map(numb => numb * 2);
 }
 
-const myNumbers = [1, 2, 3, 4];
-console.log(doubleOddNumbers(myNumbers));
+const myNumbers = [1, 2, 3, 4, 5, 6];
+console.log(doubleOddNumbers(myNumbers)); // ==> [2, 6, 10]
 
 // Do not change or remove anything below this line
 module.exports = {

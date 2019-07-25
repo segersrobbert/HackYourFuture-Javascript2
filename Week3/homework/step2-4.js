3a7a19df805bc9468ad2d0ad63cc5165d74f58af
hakanErgin <hakan.ergin@gmail.com>
2019-07-25 15:37:47 +0200

    done, except bonus

--- Week3/homework/step2-4.js
+++ Week3/homework/step2-4.js
@@ -1,10 +1,14 @@
 'use strict';
 
 function Dog() {
-  // add your code here
+  this.name = "Albert";
+  this.color = "blue";
+  this.numLegs = 2;
 }
 
 const hound = new Dog();
+console.log(hound);
+// console.log(typeof(hound));
 
 // Do not change or remove anything below this line
 module.exports = hound;

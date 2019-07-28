94a6a850adac0cab16c9f1946100c70aa9195e12
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-07-28 15:23:38 +0200

    Update step2-2.js

--- Week3/homework/step2-2.js
+++ Week3/homework/step2-2.js
@@ -3,18 +3,22 @@
 function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
   const numbers = [];
 
-  // Replace this comment and the next line with your code
-  console.log(startIndex, stopIndex, threeCallback, fiveCallback, numbers);
+  for(let i = startIndex; i<stopIndex+1; i++){
+    numbers.push(i);
+  }
+  
+  numbers.map(item => { if(item%3===0){threeCallback(item)} if(item%5===0){fiveCallback(item)}})
+  
 }
 
 function sayThree(number) {
   // Replace this comment and the next line with your code
-  console.log(number);
+  console.log("Three");
 }
 
 function sayFive(number) {
   // Replace this comment and the next line with your code
-  console.log(number);
+  console.log("Five");
 }
 
 threeFive(10, 15, sayThree, sayFive);

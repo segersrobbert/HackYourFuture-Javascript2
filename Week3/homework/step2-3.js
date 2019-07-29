694be960285d023403ed181e38bddef47caf3d92
ebruispir <50908568+ebruispir@users.noreply.github.com>
2019-07-29 22:40:49 +0200

    Update step2-3.js

--- Week3/homework/step2-3.js
+++ Week3/homework/step2-3.js
@@ -4,7 +4,9 @@
 function repeatStringNumTimesWithFor(str, num) {
   // eslint-disable-next-line prefer-const
   let result = '';
-
+  for (; num > 0; num--) {
+    result += str;
+  }
   // Replace this comment and the next line with your code
   console.log(str, num, result);
 
@@ -17,8 +19,10 @@ console.log('for', repeatStringNumTimesWithFor('abc', 3));
 function repeatStringNumTimesWithWhile(str, num) {
   // eslint-disable-next-line prefer-const
   let result = '';
-
-  // Replace this comment and the next line with your code
+  while (num > 0) {
+    result += str;
+    num--;
+  }
   console.log(str, num, result);
 
   return result;
@@ -30,8 +34,36 @@ console.log('while', repeatStringNumTimesWithWhile('abc', 3));
 function repeatStringNumTimesWithDoWhile(str, num) {
   // eslint-disable-next-line prefer-const
   let result = '';
+  do {
+    result += str;
+    num--;
+  } while (num > 0);
 
-  // Replace this comment and the next line with your code
+  console.log(str, num, result);
+
+  return result;
+}
+
+console.log('do-while', repeatStringNumTimesWithDoWhile('abc', 3));
+
+// Do not change or remove anything below this line
+module.exports = {
+  repeatStringNumTimesWithFor,
+  repeatStringNumTimesWithWhile,
+  repeatStringNumTimesWithDoWhile,
+};
+
+console.log('while', repeatStringNumTimesWithWhile('abc', 3));
+
+// Use a 'do...while' loop
+function repeatStringNumTimesWithDoWhile(str, num) {
+  // eslint-disable-next-line prefer-const
+  let result = '';
+
+  do {
+    result += str;
+    num--;
+  } while (num > 0);
   console.log(str, num, result);
 
   return result;

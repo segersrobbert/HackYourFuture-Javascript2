1fc63a54c01ae85a2b1ca93b3ddc7008100d64da
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-08-03 20:55:03 +0200

    Update step2-3.js

--- Week3/homework/step2-3.js
+++ Week3/homework/step2-3.js
@@ -5,8 +5,9 @@ function repeatStringNumTimesWithFor(str, num) {
   // eslint-disable-next-line prefer-const
   let result = '';
 
-  // Replace this comment and the next line with your code
-  console.log(str, num, result);
+  for(let i = 0; i < num; i++){
+   result+=str; 
+  }
 
   return result;
 }
@@ -17,9 +18,11 @@ console.log('for', repeatStringNumTimesWithFor('abc', 3));
 function repeatStringNumTimesWithWhile(str, num) {
   // eslint-disable-next-line prefer-const
   let result = '';
-
-  // Replace this comment and the next line with your code
-  console.log(str, num, result);
+  let i = 0;
+  while(i<num){
+   result+=str;
+    i++;
+  }
 
   return result;
 }
@@ -30,9 +33,12 @@ console.log('while', repeatStringNumTimesWithWhile('abc', 3));
 function repeatStringNumTimesWithDoWhile(str, num) {
   // eslint-disable-next-line prefer-const
   let result = '';
-
-  // Replace this comment and the next line with your code
-  console.log(str, num, result);
+  let i = 0;
+  
+  do{
+    result+=str;
+    i++;
+  }while(i<num)
 
   return result;
 }

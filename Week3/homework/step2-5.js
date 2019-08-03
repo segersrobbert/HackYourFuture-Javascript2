a2c9ed7c13c799745a5b2520a4c575bb93047fc4
Mesut <50662174+Efiloglu@users.noreply.github.com>
2019-08-03 23:13:52 +0200

    Update step2-5.js

--- Week3/homework/step2-5.js
+++ Week3/homework/step2-5.js
@@ -3,7 +3,11 @@
 function multiplyAll(arr) {
   // eslint-disable-next-line
   let product = 1;
-
+  for(var i=0; i < arr.length; i++){
+    for (var j=0; j < arr[i].length; j++){
+      product = product * arr[i][j];
+    }
+  }
   // Replace this comment and the next line with your code
   console.log(arr, product);
 

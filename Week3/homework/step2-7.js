699380a908002c1a4834c334b6881bae7d8be794
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-08-03 21:14:27 +0200

    Update step2-7.js

--- Week3/homework/step2-7.js
+++ Week3/homework/step2-7.js
@@ -21,3 +21,9 @@ f2(y);
 console.log(y);
 
 // Add your explanation as a comment here
+/*
+For f1() the parameter is given to be variable. Therefore, f1(x) copies the given value x rather than referencing to x directly. 
+Whereas for f2() the parameter is an object. And objects reference values. Thus when calculating, the value is directly referenced to y.
+and from there to x, changing the original value.
+
+*/

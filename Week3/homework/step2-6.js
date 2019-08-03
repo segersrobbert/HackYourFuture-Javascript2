92df197790a77095022f916e3375db168ef91593
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-08-03 21:06:40 +0200

    Update step2-6.js

--- Week3/homework/step2-6.js
+++ Week3/homework/step2-6.js
@@ -4,13 +4,29 @@ const arr2d = [[1, 2], [3, 4], [5, 6]];
 const arr3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
 
 function flattenArray2d(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+  let farr = [];
+  
+  for(let firstd of arr){
+    for (let secd of firstd){
+      farr.push(secd);
+    }
+  }
+  
+  return farr;
 }
 
 function flattenArray3d(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+  let farr = [];
+  
+  for(let firstd of arr){
+    for (let secd of firstd){
+      for(let thirdd of secd){
+      farr.push(secd);
+      }
+    }
+  }
+  
+  return farr;
 }
 
 console.log(flattenArray2d(arr2d)); // -> [1, 2, 3, 4, 5, 6]
